354bf87c337579a2792f99ea7f161163eeed666e
Your Name <you@example.com>
2014-04-12 09:05:36 -0600

    Add lunarPhase lunar phase otro mas

--- NASALunarPhase/lunarphase.php
+++ NASALunarPhase/lunarphase.php
@@ -22,63 +22,54 @@
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
-                    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+            <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
             <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
             <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
             <script>
                 $(document).ready(function(){
-                    $("#datepicker" ).datepicker();
+                    $data = $("#datepicker").datepicker();
                 });
             </script>
 	</head>
 	<body>
-		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">NASA Lunar Phase</a>
-				</div>
-				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#contact">Contact</a></li>
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div>
-		</div>
-
-		<!--<div class="imageBG">
-			<div class="container">
-				<div class="starter-template">
-					<h1>Ciclo Lunar</h1>
-					<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-				</div>
-			</div>
-		</div>--><!-- /.container -->
+            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                    <div class="container">
+                            <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                    </button>
+                                    <a class="navbar-brand" href="#">NASA Lunar Phase</a>
+                            </div>
+                            <div class="collapse navbar-collapse">
+                                    <ul class="nav navbar-nav">
+                                            <li class="active"><a href="#">Home</a></li>
+                                            <li><a href="#about">About</a></li>
+                                            <li><a href="#contact">Contact</a></li>
+                                    </ul>
+                            </div><!--/.nav-collapse -->
+                    </div>
+            </div>
 		<div class="container">
-			<div class="stater-template">
-				<?php
-					if($_POST):
-                                                $date = $_POST['date'];
-						echo "<div class='starter-template'>";
-						include './class/LunarPhases.php';
-						echo "</div>";
-					else:
-				?>
-					<form method="post" action="lunarphase.php">
-                                            <input type="text" id="datepicker" name="date" />
-                                            <input type="submit" name="enviar" value="Calcular"/>
-					</form>
-				<?php
-					endif;
-				?>				
-			</div>
+                    <div class="stater-template">
+                        <?php
+                        if($_POST):
+                                $date = $_POST['date'];
+                                echo "<div class='starter-template'>";
+                                include './class/LunarPhases.php';
+                                echo "</div>";
+                        else:
+                        ?>
+                        <form method="post" action="lunarphase.php">
+                            <input id="datepicker" type="text" name="date" />
+                            <input type="submit" name="enviar" value="Calcular"/>
+                        </form>
+                        <?php
+                        endif;
+                        ?>				
+                    </div>
 		</div>
 
 		<!-- Bootstrap core JavaScript
